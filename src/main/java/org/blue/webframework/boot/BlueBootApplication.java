638421c4cbabf1efea45bf5a2ba77adc411cee62
Long <simon2007@163.com>
2019-01-07 10:56:34 +0800

    合并mybatis到bootapplication中

--- src/main/java/org/blue/webframework/boot/BlueBootApplication.java
+++ src/main/java/org/blue/webframework/boot/BlueBootApplication.java
@@ -3,6 +3,7 @@ package org.blue.webframework.boot;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.Executors;
@@ -10,12 +11,16 @@ import java.util.concurrent.ScheduledExecutorService;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.plugin.Interceptor;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -27,6 +32,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.github.pagehelper.PageHelper;
 
 @EnableTransactionManagement
 @ComponentScan(value = "org.blue.webframework", lazyInit = true, excludeFilters = { @Filter(Controller.class),
@@ -99,5 +105,57 @@ public class BlueBootApplication extends SpringBootServletInitializer {
 		druidDataSource.setConnectProperties(properties);
 
 		return druidDataSource;
+	}
+
+	private final String mapperLocations = "classpath*:org/blue/webframework/**/mapper/*.xml";
+
+	@Bean
+	@ConditionalOnMissingBean // 当容器里没有指定的Bean的情况下创建该对象
+	public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource) throws IOException {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		// 设置数据源
+		sqlSessionFactoryBean.setDataSource(dataSource);
+		// 设置mybatis的主配置文件
+
+		sqlSessionFactoryBean.setMapperLocations(getMyBatisMapperLocations(mapperLocations).toArray(new Resource[0]));
+
+		sqlSessionFactoryBean.setPlugins(new Interceptor[] { createPageHelper() });
+		sqlSessionFactoryBean.setConfigurationProperties(createMyBatisConfigProperties());
+
+		return sqlSessionFactoryBean;
+	}
+
+	protected Properties createMyBatisConfigProperties() {
+		Properties properties = new Properties();
+		properties.setProperty("logImpl", "LOG4J");
+		// 驼峰转换
+		properties.setProperty("mapUnderscoreToCamelCase", "true");
+		return properties;
+	}
+
+	protected PageHelper createPageHelper() throws IOException {
+		PageHelper pageHelper = new PageHelper();
+		pageHelper.setProperties(loadProperties("classpath:properties/pageHeper.properties"));
+		return pageHelper;
+	}
+
+	protected List<Resource> getMyBatisMapperLocations(String locationPattern) throws IOException {
+		System.out.println("getMapperLocations " + locationPattern);
+		List<Resource> ret = new ArrayList<Resource>();
+		for (String path : locationPattern.split(","))
+			ret.addAll(Arrays.asList(resolver.getResources(path)));
+		return ret;
+	}
+
+	protected String getMyBatisMapperBasePackage() {
+		return "org.blue.webframework.sys.*.dao,org.blue.webframework.biz.*.dao";
+	}
+
+	@Bean
+	@ConditionalOnMissingBean // 当容器里没有指定的Bean的情况下创建该对象
+	public MapperScannerConfigurer mapperScannerConfigurer() {
+		MapperScannerConfigurer config = new MapperScannerConfigurer();
+		config.setBasePackage(getMyBatisMapperBasePackage());
+		return config;
 	}
 }
--- src/test/java/org/blue/housekeeper/boot/HouseKeeperBoot.java
+++ src/test/java/org/blue/housekeeper/boot/HouseKeeperBoot.java
@@ -1,13 +1,27 @@
 package org.blue.housekeeper.boot;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.blue.webframework.boot.BlueBootApplication;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
 
 @SpringBootConfiguration
 @SpringBootApplication
 @ComponentScan("org.blue.housekeeper")
 public class HouseKeeperBoot extends BlueBootApplication {
+	@Override
+	protected List<Resource> getMyBatisMapperLocations(String locationPattern) throws IOException {
+
+		return super.getMyBatisMapperLocations(locationPattern + ",classpath:org/blue/housekeeper/**/mapper/*.xml");
+	}
+
+	@Override
+	protected String getMyBatisMapperBasePackage() {
+		return super.getMyBatisMapperBasePackage() + ",org.blue.housekeeper.biz.*.dao";
+	}
 
 }
